f76dbd813a646717fd5ce770a90a4e4ac9c24357
cd032032@gmail.com <cd032032@gmail.com@d5963d09-bdce-33ed-41b0-ce3c252c775b>
2014-06-20 10:07:01 +0000

    사각형 그리기 추가

---
+++ palipalette/src/mem/yozisunji/palipalette/PailRectangle.java
@@ -0,0 +1,42 @@
+package mem.yozisunji.palipalette;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public class PailRectangle extends PaliObject {
+	float left, top, right, bottom;
+
+	PailRectangle(String tag, int scolor, int fcolor)
+	{
+		super(tag,scolor,fcolor);
+	}
+	PailRectangle(String tag, float left, float top, float right, float bottom, boolean v)
+	{
+		paint = new Paint();
+		paint.setAntiAlias(true);
+		svgtag = tag;
+		this.left = left;
+		this.top = top;
+		this.right = right;
+		this.bottom = bottom;
+		validation = v;
+	}
+	PailRectangle(String tag, float left, float top, float right, float bottom, int scolor, int fcolor)
+	{
+		svgtag = tag;
+		this.left = left;
+		this.top = top;
+		this.right = right;
+		this.bottom = bottom;
+		paint = new Paint();
+		paint.setAntiAlias(true);
+		paint.setColor(scolor);
+		paint.setStyle(Paint.Style.STROKE);
+		paint.setColor(fcolor);
+		paint.setStyle(Paint.Style.FILL);
+	}
+	
+	public void drawObject(Canvas c) {
+		c.drawRect(left, top, right, bottom, paint);
+	}
+}
--- palipalette/src/mem/yozisunji/palipalette/PaliCanvas.java
+++ palipalette/src/mem/yozisunji/palipalette/PaliCanvas.java
@@ -25,7 +25,7 @@ public class PaliCanvas extends View {
 	private Picture prepicture;
 	int width, height;
 	PictureDrawable pd;
-	public static int selectedTool=1;
+	public static int selectedTool=2;
 	String movement="";
 	String html="";
 	
@@ -129,6 +129,17 @@ public class PaliCanvas extends View {
 				 float cy = downY;
 				 html = "<circle cs="+ cx +" cy=" + cy + " r=" + r + " style=\"fill:yellow;stroke:purple;stroke-width:2\"/> ";
 				 SVGParser.objs.add(new PaliCircle(html,cx,cy,r, true));
+				 this.drawLayer(0);
+				 break;
+			 case 2: // Rectangle
+				 float width = (float)Math.sqrt((float)Math.pow(upX-downX, 2));
+				 float height = (float)Math.sqrt((float)Math.pow(upY-downY, 2));
+				 float left = downX;
+				 float top = downY;
+				 float right = upX;
+				 float bottom = upY;
+				 html = "<rect x="+downX+" y="+downY+" width="+width+" height="+height+" style=\"fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)\" /> ";
+				 SVGParser.objs.add(new PailRectangle(html,left,top,right,bottom,true));
 				 this.drawLayer(0);
 				 break;
 			 }
